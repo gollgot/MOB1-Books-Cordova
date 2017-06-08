85f95cc54c3fa4fcc56aecfc1b6faff58eff2cfb
Loïc Dessaules <loic.dessaules@cpnv.ch>
2017-06-08 09:46:34 +0200

    Added angular.js and created route and view file + navigation tabs
    I added angular because there will be a lot of user interaction, this way a front end framework will help us.
    
    I added 2 route and create views
    
    I built the navigation on the bottom tabs

---
+++ www/js/app.js
@@ -0,0 +1,15 @@
+// Cordova listener
+document.addEventListener('deviceready', function(){
+	// Do some cordova stuff, when the device is ready
+}, false);
+
+// Load angular module on our app (loaded on the <html> balise)
+var app = angular.module('app', []);
+
+// Route
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/library', {templateUrl: 'view/library.html'})
+		.when('/search', {templateUrl: 'view/search.html'})
+		.otherwise({redirectTo: '/library'});
+});
---
+++ www/js/controller/navController.js
@@ -0,0 +1,6 @@
+// The scope allows to pass variables to the view
+function navController($scope){
+	// Default case, the menu is active to "library"
+	$scope.menu = 'library';
+	// And on the HTML, when we clic on each nav link, the active class and the variable change
+}
